04e958b57325cec9fb91894158872c68ee851a62
BlackBracken <me@bracken.black>
2019-12-05 19:52:15 +0900

    greet with configuration

--- src/Application.kt
+++ src/Application.kt
@@ -2,44 +2,17 @@ package black.bracken
 
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.features.ContentNegotiation
-import io.ktor.gson.gson
-import io.ktor.http.ContentType
-import io.ktor.http.content.resources
-import io.ktor.http.content.static
-import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import io.ktor.server.netty.EngineMain
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main() = EngineMain.main(arrayOf())
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-    install(Authentication) {
-    }
-
-    install(ContentNegotiation) {
-        gson {
-        }
-    }
-
+fun Application.module() {
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        // Static feature. Try to access `/static/ktor_logo.svg`
-        static("/static") {
-            resources("static")
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
+            call.respondText("Hi.")
         }
     }
 }
-
