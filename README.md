41029fb4a168d8456fd21c583e2f3235d48122ae
BlackBracken <me@bracken.black>
2019-12-07 22:55:03 +0900

    deploy database

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ## Play
 1. Clone this repo & move to the workspace.
-2. `$ ./gradlew shadowJar`
-3. `$ docker build -t jukepot-server .`
-4. Write variables for docker-compose into `.env`.
-5. `$ docker-compose build`
-6. `$ docker-compose up -d`
+2. Write variables for docker-compose into `.env`.
+3. Run as written below.
+```sh
+./gradlew shadowJar
+docker-compose build
+docker-compose up -d
+```
--- src/Application.kt
+++ src/Application.kt
@@ -6,13 +6,34 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insertIgnore
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.random.Random
 
 fun main() = EngineMain.main(arrayOf())
 
+object Numbers : Table() {
+    val number = integer("number")
+}
+
 fun Application.module() {
+    Database.connect("jdbc:mariadb://jukepot-mariadb/jukebox", "org.mariadb.jdbc.Driver", "user", "password")
+
+    transaction {
+        SchemaUtils.create(Numbers)
+    }
+
     routing {
         get("/") {
             call.respondText("Hi.")
+            transaction {
+                Numbers.insertIgnore {
+                    it[number] = Random.nextInt()
+                }
+            }
         }
     }
 }
