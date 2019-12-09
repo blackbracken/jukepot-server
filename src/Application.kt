2e28ae7922530fb28198108d185501b7ec6acdb4
BlackBracken <me@bracken.black>
2019-12-09 14:25:12 +0900

    block to start until app get connection to db

--- src/Application.kt
+++ src/Application.kt
@@ -15,6 +15,10 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insertIgnore
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+import java.util.*
 import kotlin.random.Random
 
 private const val ENV_DATABASE = "jukepot.database"
@@ -23,16 +27,18 @@ fun main() = EngineMain.main(arrayOf())
 
 @KtorExperimentalAPI
 fun Application.module() {
-    // TODO: don't sleep on app
-    Thread.sleep(45_000)
-
     with(environment.config) {
         val dbHost = propertyOrNull("$ENV_DATABASE.host")?.getString() ?: "localhost"
         val dbDatabase = propertyOrNull("$ENV_DATABASE.database")?.getString() ?: "jukepot"
         val dbUser = property("$ENV_DATABASE.user").getString()
         val dbPassword = property("$ENV_DATABASE.password").getString()
 
-        Database.connect("jdbc:mariadb://$dbHost/$dbDatabase", "org.mariadb.jdbc.Driver", dbUser, dbPassword)
+        val url = "jdbc:mariadb://$dbHost/$dbDatabase"
+        while (getConnectionOrNull(url, dbUser, dbPassword)?.also(Connection::close) == null) {
+            Thread.sleep(1_000)
+        }
+
+        Database.connect(url, "org.mariadb.jdbc.Driver", dbUser, dbPassword)
     }
 
     transaction {
@@ -53,6 +59,16 @@ fun Application.module() {
         }
     }.start(wait = true)
 }
+
+private fun getConnectionOrNull(url: String, user: String, password: String): Connection? =
+    try {
+        DriverManager.getConnection(url, Properties().apply {
+            put("user", user)
+            put("password", password)
+        })
+    } catch (ignored: SQLException) {
+        null
+    }
 
 object Numbers : Table() {
     val number = integer("number")
